a658ee65fb2dd2d6741d193830bdfe9cb4380aa4
twinklinglittlestars <107315845+twinklinglittlestars@users.noreply.github.com>
2022-11-04 19:44:08 -0400

    layout xml code

--- Assets/Prospector/__Scripts/Prospector.cs
+++ Assets/Prospector/__Scripts/Prospector.cs
@@ -11,13 +11,16 @@ public class Prospector : MonoBehaviour {
 
 	[Header("Set in Inspector")]
 	public TextAsset			deckXML;
+	public TextAsset			layoutXML;
+
 
 
 	[Header("Set Dynamically")]
 	public Deck					deck;
+	public Layout               layout;
 
 	void Awake(){
-		S = this;
+		S = this; // Set up a singleton for prospector
 	}
 
 	void Start() {
@@ -25,12 +28,16 @@ public class Prospector : MonoBehaviour {
 		deck.InitDeck (deckXML.text); // Pass DeckXML to it
 		Deck.Shuffle(ref deck.cards); // This shuffles the deck by reference 
 
-		Card c;
-		for (int cNum = 0; cNum<deck.cards.Count; cNum++)
-        {
-			c = deck.cards[cNum];
-			c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
-        }
+		// this section can be commented out; we're working on a real layout now
+		//Card c;
+		//for (int cNum = 0; cNum<deck.cards.Count; cNum++)
+        //{
+		//	c = deck.cards[cNum];
+		//	c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
+        //}
+
+		layout = GetComponent<Layout>(); // Get the Layout component
+		layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
 	}
 
 }
